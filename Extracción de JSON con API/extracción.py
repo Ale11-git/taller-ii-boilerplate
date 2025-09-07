1d5450c8b504fff22497b0e4f718cc20d788c0cd
Luciano Tartarini <tartarluc@gmail.com>
2025-09-07 19:07:55 -0300

    Avances de extraccion mediante API

---
+++ Extracción de JSON con API/extracción.py
@@ -0,0 +1,30 @@
+import requests
+
+response = requests.get('https://dummyjson.com/products')
+
+if response.status_code == 200:
+    data = response.json()
+    products = data['products']   # lista de productos
+
+    for product in products:
+        print("ID:", product.get('id', 'Sin ID'))
+        print("Nombre:", product.get('title', 'Sin nombre'))
+        print("Precio:", product.get('price', 'Sin precio'))
+        print("Marca:", product.get('brand', 'Sin marca'))   # <- cambio acá
+        print("Categoría:", product.get('category', 'Sin categoría'))
+
+        reviews = product.get("reviews", []) #lista de reseñas
+        if reviews:
+            print("Reseñas:")
+
+            for review in reviews:
+                print("-- Usuario: ", review.get('reviewerName'))
+                print("-- Valoracion: ", review.get('rating'))
+                print("-- Commentario: ", review.get('comment'))
+        else:
+            print("Reseñas: (sin reseñas)")
+
+        print("-" * 40)
+
+else:
+    print("Error:", response.status_code)
